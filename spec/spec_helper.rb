2a12fb326f93bbac0734aa9809b9106cc88ca584
Matthew Edge <matt.edge@levvel.io>
2016-10-07 11:27:49 -0400

    Hiera config added for tests. Relies on spec/fixtures/hiera/default.yaml currently

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,4 +5,7 @@ fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
 RSpec.configure do |c|
   c.module_path = File.join(fixture_path, 'modules')
   c.manifest_dir = File.join(fixture_path, 'manifests')
+
+  # Setup Hiera
+  c.hiera_config = File.expand_path(File.join(__FILE__, '../fixtures/hiera.yaml'))
 end
